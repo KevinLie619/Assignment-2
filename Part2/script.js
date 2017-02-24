914194c5d25c3f7ad0ab6c7282d93e1b30f205af
KevinLie619 <kevinlie619@gmail.com>
2017-02-23 17:44:25 -0800

    uploaded files from computer

---
+++ Part2/script.js
@@ -0,0 +1,193 @@
+//1
+var x=-15
+console.log(Math.abs(x));
+//2
+var x=.74854
+console.log(Math.ceil(x));
+//3 
+var x=.34585
+console.log(Math/floor(x));
+//4
+
+//5
+console.log(Math.pow(16,16));
+
+//6
+var day = new Date();
+console.log(d.toDateString());
+
+
+//7 
+var day=new Date();
+console.log(d.getDate());
+//8
+
+var day=new Date();
+console.log(d.toString())
+
+//9
+if {var day="0-4"
+   
+   console.log(day.true)
+   }
+else{
+	var end="5-6"
+	console.log(day.false)
+	
+}
+	
+//10
+var day=new Date();
+console.log(d.getDay(-1));
+
+//11
+var day=new Date();
+console.log(d.toDateString()charAt(0));
+//12
+//13 
+var people [ursula paul henry tabitha lucy]
+var grades [80 77 88 95 68]
+for var x people{
+	console.log(person[x])
+} 
+//14
+var x=0
+while (x=2,4,6,8,10,12,14){
+	x++
+console.log("even")
+while (x=1,3,5,7,9,11,13,15)
+	x++
+	console.log("odd")
+}
+//15
+var x=0
+while (x <100){
+	x+++;
+	console.log("fizz");
+	x+++++;
+	console.log("bizz")
+	x+++,x+++++;
+	console.log ("fizzbizz")
+	
+}
+
+
+//part4
+var welcome=prompt("You want to play") 
+
+ if (welcome=yes){
+	 var gameon = alert("awesome our hero is waiting")
+ }
+	else (welcome=no){
+		var nochoice = alert("Too bad,were going to play anyway because our hero desperately needs your help!")
+	}
+var drown = alert("You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall…”") 
+var direction =prompt("which direction would you like to head. forward left or right")
+switch (direction) {
+	case "forward":
+		alert("you walk about 100 yards and safely make your way our of the cave")
+		break;
+	case "left"
+		alert("your thirst has gotten the better of you. You trip on a rock hit your head and fall into a pool of water and drown")
+		break;
+	case "right"
+		alert("You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever")
+	case "deffault"
+		alert("“Finally, ask the user to rate your game on a scale of 1 to 10. If the feedback score is between 6 and 10, display the message “Thank you, we will continue to make improvements to the game")
+		
+	var results 
+		alert("!” If the feedback score is between 1 and 5, display the message “Whatever, you weren’t very good at this game anyway!”")
+}
+
+
+
+
+//Part 5//// 
+
+var gameWelcome = alert("Welcome to the coin toss game!");
+var userChoice = prompt("Do you choose HEADS or TAILS?").toUpperCase();
+var coinflip = Math.random();
+if (userChoice === "HEADS") {
+    if (coinflip < 0.5) {
+        var result = alert("The coin landed on heads. You Win!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Lose!");
+    }
+}
+else {
+    if (coinToss < 0.5) {
+        var result = alert("The coin landed on heads. You Lose!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Win!");
+    }
+}
+		
+
+		
+//		part 6 
+
+var gameWelcome = alert("Welcome to the coin toss game!");
+var userChoice = prompt("Do you choose HEADS or TAILS?").toUpperCase();
+var coinflip = Math.random();
+for (var i = 1; i <= 10; i++) {
+	console.log(i);
+ 
+
+if (userChoice === "HEADS") {
+    if (coinflip < 0.5) {
+        var result = alert("The coin landed on heads. You Win!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Lose!");
+    }
+}
+else {
+    if (coinToss < 0.5) {
+        var result = alert("The coin landed on heads. You Lose!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Win!");
+    }
+}
+		
+//part 7
+		
+		
+var gameWelcome = alert("Welcome to the coin toss game!");
+var userChoice = prompt("Do you choose HEADS or TAILS?").toUpperCase();
+var coinflip = Math.random();
+if (userChoice === "HEADS") {
+    if (coinflip < 0.5) {
+        var result = alert("The coin landed on heads. You Win!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Lose!");
+    }
+}
+else {
+    if (coinToss < 0.5) {
+        var result = alert("The coin landed on heads. You Lose!");
+    }
+    else {
+        var result = alert("The coin landed on tails. You Win!");
+    }
+}
+//part 8
+		 {
+    var string = "";
+   
+    for (string = 1; stromg < 7; string++) {
+        if (i === 2 || i === 3) continue;
+        
+    }
+}
+		
+		part 9
+		while (x=2,4,6,8,10,12,14){
+	x++
+console.log("even")
+while (x=1,3,5,7,9,11,13,15)
+	x++
+	console.log("odd")
---
+++ script.js
@@ -0,0 +1,36 @@
+//1
+var name = "kevin";
+alert(name.length);
+//2
+var name = "kevin";
+alert(name.charCodeAt(0));
+//3
+var first = "kevin";
+var last = "lai";
+alert(first.concat(last));
+//4
+var fox="The quick brown fox jumps over the lazy dog"
+
+alert(fox.slice(0,3));
+//5 
+var fox="The quick brown fox jumps over the lazy dog"
+alert(fox.lastindex.of("fox"));
+//6
+var fox="The quick brown fox jumps over the lazy dog"
+alert(fox.replace("lazy dog", "kevin"));
+//7
+var fox="The quick brown fox jumps over the lazy dog"
+alert(fox.search(/kevin/g));
+//8
+var old_string="The quick brown fox jumps over the lazy dog"
+var new_string="ols_String.slice(25,34);
+alert(new_string);
+//9
+var fox="      THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG           "
+alert(fox.trim());
+console.log(fox.toLowerCase());
+//10
+var fox="The quick brown fox jumps over the lazy dog"
+charat(0),toUpperCase(0);
+alert(fox.charset.toUpperCase)''
+
